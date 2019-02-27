fe12b8ec9b4f9e1053cf41d7dc22ec48c8bfcf90
tjark.uschakow <tjark.uschakow@WS-018.cybertron.global>
2019-02-27 11:52:30 +0100

    LazyPagFilterTable LazyFilter fast fertig

--- src/main/java/beta/web/presenter/LazyContactDataModel.java
+++ src/main/java/beta/web/presenter/LazyContactDataModel.java
@@ -5,10 +5,12 @@
  */
 package beta.web.presenter;
 
-import beta.server.eao.ContactEao;
 import beta.server.entity.Contact;
+import beta.server.entity.Sex;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import org.primefaces.model.LazyDataModel;
 import org.primefaces.model.SortOrder;
 import org.slf4j.Logger;
@@ -23,26 +25,112 @@ public class LazyContactDataModel extends LazyDataModel<Contact> {
     private final Logger L = LoggerFactory.getLogger(LazyContactDataModel.class);
     
     private List<Contact> data;
-
+    
     public LazyContactDataModel(List<Contact> data) {
         this.data = data;
         this.setRowCount(this.data.size());
         L.info("LazyModelCreated");
     }
-
+    
     @Override
     public List<Contact> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object> filters) {
         L.info("inside Load");
         
+        List<Contact> filteredList;
         
-        for(Map.Entry<String,Object> entry: filters.entrySet()) {
-            L.info("Key:{}",entry.getKey());
-            L.info("Value:{}",entry.getValue());
-                
+        filteredList = filterList(filters);
+        
+        this.setRowCount(filteredList.size());
+        
+        if (this.getRowCount() < this.getPageSize()) {
+            this.setPageSize(this.getRowCount());
+            return filteredList;
+        } else {
+            return filteredList.subList(first, first + pageSize);
         }
         
-        List<Contact> list = data.subList(first, first + pageSize);
-        return list;
     }
 
+    /**
+     * Filter the DataList and return the filtered list
+     *
+     * @param filters
+     * @return
+     */
+    public List<Contact> filterList(Map<String, Object> filters) {
+        List<Contact> filteredList = new ArrayList<>(data);
+        L.info("inside Filter");
+        L.info("Filter: {}",filters);
+        for (Map.Entry<String, Object> entry : filters.entrySet()) {
+            
+            switch (entry.getKey()) {
+                case "title":
+                    filteredList = filteredList.stream()
+                            .filter(cont -> cont.getTitle().equals(entry.getValue()))
+                            .collect(Collectors.toList());
+                    break;
+                case "firstName":
+                    filteredList = filteredList.stream()
+                            .filter(cont -> cont.getFirstName().contains((String) entry.getValue()))
+                            .collect(Collectors.toList());
+                    break;
+                case "lastName":
+                    filteredList = filteredList.stream()
+                            .filter(cont -> cont.getLastName().contains((String) entry.getValue()))
+                            .collect(Collectors.toList());
+                    break;
+                case "translateSex(contact)":
+                    filteredList = filteredList.stream()
+                            .filter(cont -> cont.getSex().equals(((String) entry.getValue()).equals("männlich") ? Sex.MALE : Sex.FEMALE))
+                            .collect(Collectors.toList());
+                    break;
+                case "streetsToString(contact)":
+                    filteredList = filteredList.stream()
+                            .filter(cont -> cont.getAddresses().stream()
+                            .map(add -> add.getStreet())
+                            .collect(Collectors.joining())
+                            .contains((String) entry.getValue()))
+                            .collect(Collectors.toList());
+                    break;
+                case "zipCodeToString(contact)":
+                    filteredList = filteredList.stream()
+                            .filter(cont -> cont.getAddresses().stream()
+                            .map(add -> add.getZipCode())
+                            .collect(Collectors.joining())
+                            .contains((String) entry.getValue()))
+                            .collect(Collectors.toList());
+                    break;
+                case "cityToString(contact)":
+                    filteredList = filteredList.stream()
+                            .filter(cont -> cont.getAddresses().stream()
+                            .map(add -> add.getCity())
+                            .collect(Collectors.joining())
+                            .contains((String) entry.getValue()))
+                            .collect(Collectors.toList());
+                    break;
+                case "countryToString(contact)":
+                    filteredList = filteredList.stream()
+                            .filter(cont -> cont.getAddresses().stream()
+                            .map(add -> add.getCountry().name())
+                            .collect(Collectors.joining())
+                            .contains((String) entry.getValue()))
+                            .collect(Collectors.toList());
+                    break;
+                case "communicationsToString(contact)":
+                    filteredList = filteredList.stream()
+                            .filter(cont -> cont.getCommunications().stream()
+                            .map(com -> com.getType().name() + " " + com.getIdentifier())
+                            .collect(Collectors.joining())
+                            .contains((String) entry.getValue()))
+                            .collect(Collectors.toList());
+                    break;
+                default:
+                    throw new AssertionError("Error with " + entry.getKey()+" and " + entry.getValue());
+            }
+            
+        }
+        
+        return filteredList;
+    }
+    
 }
